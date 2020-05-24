ab7e5cfcd60df354c39bb692598aa4507d39eaa6
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-05-24 01:15:49 -0500

    Missing files added

--- src/controller/User.js
+++ src/controller/User.js
@@ -6,7 +6,7 @@ const UserModel = require("../model/User");
 const CodeModel = require("../model/Code");
 
 module.exports = class User {
-	async login(payload) {
+    async login(payload) {
         try {
             await DB.connect();
 
@@ -60,17 +60,17 @@ module.exports = class User {
         }
     }
 
-	async validate(payload) {
+    async validate(payload) {
         try {
             await DB.connect();
 
-						// Verificamos que existe un código asociado al email
-						const code = await CodeModel.findOne({
-								email: payload.email,
-								code: payload.code
-						});
+            // Verificamos que existe un código asociado al email
+            const code = await CodeModel.findOne({
+                    email: payload.email,
+                    code: payload.code
+            });
 
-						// De no existir retornamos una respuesta con el mensaje correspondiente
+            // De no existir retornamos una respuesta con el mensaje correspondiente
             if (!code) {
                 return {
                     statusCode: 200,
@@ -82,13 +82,13 @@ module.exports = class User {
                 }
             }
 
-						// Si todo esta correcto obtenemos la información del User
-						// Generamos un token con la información del usuario (Válido por 1 día)
-						// Firmamos el payload del token con JWT y el secret de nuestras variables de entorno
-						// Finalmente enviamos como response el nuevo token
+            // Si todo esta correcto obtenemos la información del User
+            // Generamos un token con la información del usuario (Válido por 1 día)
+            // Firmamos el payload del token con JWT y el secret de nuestras variables de entorno
+            // Finalmente enviamos como response el nuevo token
             const user = await UserModel.findOne({ email: payload.email });
 						
-						const tokenPayload = {
+			const tokenPayload = {
                 id: user.id,
                 email: payload.email
             }
